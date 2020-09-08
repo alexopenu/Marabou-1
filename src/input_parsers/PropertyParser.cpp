b274aef8e6357db2c30b4031a7c699913c0191c5
Alex Usvyatsov <alexus.openu@gmail.com>
2020-09-08 21:58:15 +0300

    Minor

--- src/input_parsers/PropertyParser.cpp
+++ src/input_parsers/PropertyParser.cpp
@@ -148,8 +148,11 @@ void PropertyParser::processSingleLine( const String &line, InputQuery &inputQue
 
             if ( nlr->getNumberOfLayers() < layerIndex )
                 throw InputParserError( InputParserError::HIDDEN_VARIABLE_DOESNT_EXIST_IN_NLR );
+            printf ("Layer:  %d ; Node: %d ", layerIndex, nodeIndex);
 
             const NLR::Layer *layer = nlr->getLayer( layerIndex );
+            printf ("Layer:  %d ; Node: %d ", layerIndex, nodeIndex);
+
             if ( layer->getSize() < nodeIndex || !layer->neuronHasVariable( nodeIndex ) )
                 throw InputParserError( InputParserError::HIDDEN_VARIABLE_DOESNT_EXIST_IN_NLR );
 

770d36a23211cc8eb1540de1ebee9955c2cf4d7e
alexus <alexus.openu@gmail.com>
2020-09-30 12:12:50 +0300

    Gurobi script.

--- prepare_gurobi.sh
+++ prepare_gurobi.sh
@@ -1,11 +1,6 @@
-#!/usr/bin/csh
+#!/usr/bin/bash
 
-setenv GUROBI_HOME "/cs/labs/guykatz/alexus/gurobi903/linux64/"
-setenv PATH "${PATH}:${GUROBI_HOME}/bin"
-if (DEFINED LD_LIBRARY_PATH) then
-    setenv LD_LIBRARY_PATH "${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"
-else
-    setenv LD_LIBRARY_PATH "${GUROBI_HOME}/lib"
-endif
-setenv GRB_LICENSE_FILE "/cs/share/etc/license/gurobi/gurobi.lic"
-
+export GUROBI_HOME="PATH_TO_YOUR_GUROBI"
+export PATH="${PATH}:${GUROBI_HOME}/bin"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"
+export GRB_LICENSE_FILE="/cs/share/etc/license/gurobi/gurobi.lic"

da2b059bac673a07273c067ba3f7b342a8fd24a8
alexus <alexus.openu@gmail.com>
2020-09-30 11:00:15 +0300

    Add gurobi preparation.

---
+++ prepare_gurobi.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/csh
+
+export GUROBI_HOME="PATH_TO_YOUR_GUROBI"
+export PATH="${PATH}:${GUROBI_HOME}/bin"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"
+export GRB_LICENSE_FILE="/cs/share/etc/license/gurobi/gurobi.lic"
+

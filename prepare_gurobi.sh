da158a9e971e902a5244c987f743df1505484647
alexus <alexus.openu@gmail.com>
2020-09-30 12:42:23 +0300

    Gurobi script.

--- prepare_gurobi.sh
+++ prepare_gurobi.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-export GUROBI_HOME="PATH_TO_YOUR_GUROBI"
+export GUROBI_HOME="/cs/labs/guykatz/alexus/gurobi903/linux64"
 export PATH="${PATH}:${GUROBI_HOME}/bin"
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"
 export GRB_LICENSE_FILE="/cs/share/etc/license/gurobi/gurobi.lic"
